79c09e18cd0b9f1b5301c74d3b3fa4025dd4cd61
Derick Cardin <derickc50@gmail.com>
2018-12-03 23:03:16 +0000

    Created Fibonacci-numbers & Sum all primes

---
+++ fibonacci-numbers.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stduio.h>
+
+// Array.prototype.last = function(){
+// return this[this.length-1];
+// }
+
+// Array.prototype.secondToLast = function() {
+// return this[this.length-2];
+// }
+
+// function sumFibs(num) {
+// var sequence = [1,1];
+// while (sequence.secondToLast() + sequence.last() <= num){
+// sequence.push(sequence.secondToLast() + sequence.last());
+// }
+// return sequence.filter(function(num) {
+// return num % 2 !== 0;
+// }).reduce(function(a,b) {
+// return a + b;
+// });
+// }
+// sumFibs(4);
+
+
---
+++ sum-all-primes.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// function sumPrimes(num) {
+//   var range = []
+// for (var i = 2; i <= num; i++){
+// if(isPrimeNumber(i)){
+// range.push(i);
+// }
+//   }
+
+// return range.reduce(function(a, b){
+// return a + b;});
+// }
+
+// function isPrimeNumber(num) {
+// for(var x =2; x < num; x++){
+// if(num % x === 0){
+// return false;
+// }
+// }
+// return true;
+// }
+
+// sumPrimes(10);
+
+
+int main(void){
+
+}
