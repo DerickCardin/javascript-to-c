6db9eeae52c31d28fb3191361136021ac5de1d00
Derick Cardin <derickc50@gmail.com>
2018-12-04 01:51:56 +0000

    updated fibonacci

--- fibonacci-numbers.c
+++ fibonacci-numbers.c
@@ -23,6 +23,28 @@
 // sumFibs(4);
 
 
-int main(void){
+int sumFibs(int num);
 
+int main()
+{
+    printf("%i\n", sumFibs(6));
+}
+
+int sumFibs(int num)
+{
+  int first = 1;
+  int second = 1;
+  int third = 2;
+  int sum = 2;
+    for (;third <= num;)
+    {
+        if(third % 2 != 0)
+            sum += third;
+
+        third = first + second;
+        first = second;
+        second = third;
+        // printf("%d\n" );
+    }
+    return sum;
 }
